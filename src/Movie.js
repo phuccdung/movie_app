993f289ddc093a9b825afbd70826844d69e0d924
phuccdung <82568398+phuccdung@users.noreply.github.com>
2021-10-20 09:24:57 +0700

    redering movie

---
+++ src/Movie.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Movie({id,year,title,summary,poster}){
+    return (
+        <h3> {title}</h3>
+    );
+      
+    
+}
+Movie.prototype={
+    id:PropTypes.number.isRequired,
+    year:PropTypes.number.isRequired,
+    title:PropTypes.string.isRequired,
+    summary:PropTypes.string.isRequired,
+    poster:PropTypes.string.isRequired
+};
+export default Movie;
